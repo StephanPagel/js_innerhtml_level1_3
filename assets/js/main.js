4433ca86bd5259a2eebe9d2b6121d1e6390b1b1d
StephanPagel <stephan.pagel84@gmail.com>
2022-04-14 14:55:31 +0200

    exercises finished

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+document.getElementById("gallery").innerHTML = "<figure>Fig.1</figure><figure>Fig.2</figure><figure>Fig.3</figure>";
+
+// document.getElementById("gallery").innerHTML += "<figure>Fig.2</figure>";
