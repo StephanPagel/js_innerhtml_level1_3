5d5e0c55916f558d86f2a95631b989f64c11e518
StephanPagel <stephan.pagel84@gmail.com>
2022-04-15 22:21:00 +0200

    corrections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
-document.getElementById("gallery").innerHTML = "<figure>Fig.1</figure><figure>Fig.2</figure><figure>Fig.3</figure>";
+let gallery = document.getElementById("gallery");
 
-// document.getElementById("gallery").innerHTML += "<figure>Fig.2</figure>";
+gallery.innerHTML = '<figure><img src="https://images.unsplash.com/photo-1647379018052-efb4a5bdedad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="forrest"><figcaption>Fig.1</figcaption></figure>';
+
+gallery.innerHTML += '<figure><img src="https://images.unsplash.com/photo-1647379018052-efb4a5bdedad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="forrest"><figcaption>Fig.2</figcaption></figure>';
+
+gallery.innerHTML += '<figure><img src="https://images.unsplash.com/photo-1647379018052-efb4a5bdedad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="forrest"><figcaption>Fig.3</figcaption></figure>';
